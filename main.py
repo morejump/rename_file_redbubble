8aa59791274f5b9e4806a90db80714c0683d0752
morejump <thaoluu>
2022-06-15 00:05:22 +0700

    Divide images into folders

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from os import listdir
-import pathlib
+import os
 from tkinter import *
 from tkinter import filedialog
-import tkinter
-import os
+from os.path import isfile, join
+import shutil
 
 
 def handleSelectFolder():
@@ -13,28 +12,34 @@ def handleSelectFolder():
     return
 
 
-def handleRenameAllFiles():
+def divideFolderImages():
+    count = 0
     folder = labelSelectedPath.cget("text")
-    for count, filename in enumerate(os.listdir(folder)):
-        if filename.lower().endswith('.png'):
-            lastOccur = filename.rfind("T-Shirt")
-            if lastOccur != -1:
-                print(f"last occur: {lastOccur}")
-                newFile = filename[:lastOccur].strip()
-                dst = f"{newFile}.png"
-                dst = f"{folder}/{dst}"
-                src = f"{folder}/{filename}"
-                try:
-                    os.rename(src, dst)
-                except:
-                    print("An exception occurred")
-                    pass
-    labelSuccess.configure(text="Successfully renamed all image files!")
+    onlyfiles = [file for file in os.listdir(folder) if isfile(join(folder, file))]
+    print(onlyfiles)
+    _numberImage = int(numberImages.get())
+    while len(onlyfiles) != 0:
+        count += 1
+        numberAddImage = 0
+        if (len(onlyfiles)) < _numberImage:
+            numberAddImage = len(onlyfiles)
+        else:
+            numberAddImage = _numberImage
+
+        newDirectory = f"acc_{count}"
+        path = os.path.join(folder, newDirectory)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        for index in range(numberAddImage):
+            shutil.move(f"{folder}/{onlyfiles[index]}", f"{path}/{onlyfiles[index]}")
+        onlyfiles = [file for file in os.listdir(folder) if isfile(join(folder, file))]
+    labelSuccess.configure(text="Successfully divided images")
     return
 
 
 window = Tk()
-window.title("Rename all image files in a specific folder")
+window.title("Divide images into folders")
 window.geometry("400x100")
 # Add a intro label
 labelIntro = Label(window, text="Select the folder:")
@@ -48,12 +53,16 @@ labelPath.grid(column=0, row=1)
 # Add a selected path label
 labelSelectedPath = Label(window, text="N/A", anchor="w")
 labelSelectedPath.grid(column=1, row=1)
-# Add a rename button
-# Add button select a folder
-btnRename = Button(window, text="Rename all files", command=handleRenameAllFiles)
-btnRename.grid(column=0, row=2)
+# add textbox get number of image per folder
+labelNumberImages = Label(window, text="Entering number of image per folder:", anchor="w")
+labelNumberImages.grid(column=0, row=2)
+numberImages = Entry(window)
+numberImages.grid(column=1, row=2)
+# Add button divide images
+btnRename = Button(window, text="Divide", command=divideFolderImages)
+btnRename.grid(column=0, row=3)
 # Add label successfully
 labelSuccess = Label(window, text="", fg="green", font=("Arial", 10))
-labelSuccess.grid(column=0, row=3)
+labelSuccess.grid(column=0, row=4)
 
 window.mainloop()
