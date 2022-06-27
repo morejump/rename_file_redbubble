980c500a7de2a25c0f5e43db1084d97e4fbbc847
morejump <thaoluu>
2022-06-27 12:32:47 +0700

    Update Including subfolder functionality

--- main.py
+++ main.py
@@ -9,34 +9,34 @@ from PIL import Image, ImageEnhance
 
 def handleSelectFolder():
     labelSuccess.configure(text="")
-    path = filedialog.askdirectory()
-    labelSelectedPath.configure(text=path)
+    root = filedialog.askdirectory()
+    labelSelectedPath.configure(text=root)
     return
 
 
-def enhanceImage(folder, filename):
+def enhanceImage(filePath):
     try:
-        print(f"Handling {filename}...")
-        filePath = f"{folder}/{filename}"
+        print(f"Handling {filePath}...")
+        # filePath = f"{folder}/{filename}"
         #  Enhance color
         image = Image.open(filePath)
         coloredImage = ImageEnhance.Color(image)
         coloredImage.enhance(2).save(filePath)
         image.close()
         # Enhance Sharpness
-        image = Image.open(filePath)
-        coloredImage = ImageEnhance.Sharpness(image)
-        coloredImage.enhance(2).save(filePath)
-        image.close()
+        # image = Image.open(filePath)
+        # coloredImage = ImageEnhance.Sharpness(image)
+        # coloredImage.enhance(2).save(filePath)
+        # image.close()
         # Enhance Brightness
-        image = Image.open(filePath)
-        coloredImage = ImageEnhance.Brightness(image)
-        coloredImage.enhance(1.1).save(filePath)
-        image.close()
+        # image = Image.open(filePath)
+        # coloredImage = ImageEnhance.Brightness(image)
+        # coloredImage.enhance(1.1).save(filePath)
+        # image.close()
 
-        print(f"Complete handling {filename}!")
+        print(f"Complete handling {filePath}!")
     except Exception as ex:
-        print(f"Ignoring handle {filename} cause by an exception: {ex}")
+        print(f"Ignoring handle {filePath} cause by an exception: {ex}")
     return
 
 
@@ -44,12 +44,24 @@ def enhanceImages():
     features = []
     executor = ThreadPoolExecutor(30)
     folder = labelSelectedPath.cget("text")
-    for count, filename in enumerate(os.listdir(folder)):
-        if filename.lower().endswith('.png'):
-            feature = executor.submit(enhanceImage, folder, filename)
-            features.append(feature)
+    if isIncludeSubFolder.get() == 0:
+        print("Handle only root folder")
+        for filename in os.listdir(folder):
+            if filename.lower().endswith('.png'):
+                filePath = f"{folder}/{filename}"
+                feature = executor.submit(enhanceImage, filePath)
+                features.append(feature)
+    else:
+        print("Including sub folder")
+        for path, subdirs, files in os.walk(folder):
+            for name in files:
+                if name.lower().endswith('.png'):
+                    filePath = f"{path}/{name}".replace("\\", "/")
+                    feature = executor.submit(enhanceImage, filePath)
+                    features.append(feature)
+    imageNumbers = len(features)
     wait(features)
-    labelSuccess.configure(text=f"Bypass images successfully")
+    labelSuccess.configure(text=f"Bypass {imageNumbers} images successfully")
     print("ALL IMAGES IS HANDLED!!!")
     return
 
@@ -60,8 +72,8 @@ def onClickBypass():
 
 
 window = Tk()
-window.title("Redbubble Bypasser - v1.2")
-window.geometry("400x100")
+window.title("Redbubble Bypasser - v1.3")
+window.geometry("400x130")
 # Add a intro label
 labelIntro = Label(window, text="Select the folder:")
 labelIntro.grid(column=0, row=0)
@@ -74,11 +86,15 @@ labelPath.grid(column=0, row=1)
 # Add a selected path label
 labelSelectedPath = Label(window, text="N/A", anchor="w")
 labelSelectedPath.grid(column=1, row=1)
+# including sub-folder
+isIncludeSubFolder = IntVar()
+cbSubFolder = Checkbutton(window, text="Including SubFolder", variable=isIncludeSubFolder)
+cbSubFolder.grid(column=0, row=2)
 # Add a rename button
 btnBypass = Button(window, text="Bypass", command=onClickBypass)
-btnBypass.grid(column=0, row=2)
+btnBypass.grid(column=0, row=3)
 # Add label successfully
 labelSuccess = Label(window, text="", font=("Arial", 10))
-labelSuccess.grid(column=0, row=3)
+labelSuccess.grid(column=0, row=4)
 
 window.mainloop()
