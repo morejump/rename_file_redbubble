5398325b490ce6585ebeff277b336285dd9392cc
morejump <thaoluu>
2022-06-29 18:54:07 +0700

    Generate Gmail

--- main.py
+++ main.py
@@ -1,99 +1,55 @@
-import os
-import uuid
 import threading
 from tkinter import *
-from tkinter import filedialog
+import uuid
+from datetime import datetime
 
 
-def handleSelectFolder():
-    labelSuccess.configure(text="")
-    path = filedialog.askdirectory()
-    labelSelectedPath.configure(text=path)
+def startGenerateGmail():
+    lastGenerate = datetime.now().strftime('%H:%M:%S')
+    labelLastTime.config(text=lastGenerate)
+    mainMail = entryMainMail.get()
+
+    uniqueString = str(uuid.uuid4()).split("-")[-1]
+
+    text = mainMail.split("@")
+    print(text)
+    newMail = f"{text[0]}+{uniqueString}@{text[1]}"
+    entryGenerateMail.delete(0, END)
+    entryGenerateMail.insert(0, newMail)
     return
 
 
-def renameFile(sourcePath, cutText):
-    filename = sourcePath.split("/")[-1]
-    lastBackSlash = sourcePath.rfind("/")
-    folder = sourcePath[:lastBackSlash].strip()
-
-    lastOccur = filename.rfind(cutText)
-    print(f"last occur: {lastOccur}")
-    if lastOccur != -1:
-        print(f"last occur: {lastOccur}")
-        newFile = filename[:lastOccur].strip()
-        dst = f"{folder}/{newFile}.png"
-        try:
-            os.rename(sourcePath, dst)
-        except FileExistsError as ex:
-            uniqueId = str(uuid.uuid4()).split("-")[-1]
-            dst = f"{folder}/{newFile}_{uniqueId}.png"
-            os.rename(sourcePath, dst)
-        except Exception as ex:
-            print(f"An exception occurred: {ex}")
-            pass
-
+def processEmail():
+    threading.Thread(target=startGenerateGmail).start()
     return
 
 
-def startRename():
-    labelSuccess.configure(text="Processing...")
-    folder = labelSelectedPath.cget("text")
-    if isIncludeSubFolder.get() == 0:
-        print("Handle only root folder")
-        for filename in os.listdir(folder):
-            if filename.lower().endswith('.png'):
-                sourcePath = f"{folder}/{filename}"
-                print(sourcePath)
-                renameFile(sourcePath, cutToText.get())
-    else:
-        print("Including sub folder")
-        for path, subdirs, files in os.walk(folder):
-            for filename in files:
-                if filename.lower().endswith('.png'):
-                    sourcePath = f"{path}/{filename}".replace("\\", "/")
-                    print(sourcePath)
-                    renameFile(sourcePath, cutToText.get())
-
-    labelSuccess.configure(text="Successfully renamed all images files!")
-    return
-
-
-def processImages():
-    threading.Thread(target=startRename).start()
+def copyEmail():
+    window.clipboard_clear()
+    window.clipboard_append(entryGenerateMail.get())
     return
 
 
 window = Tk()
-window.title("Rename images v1.2")
-window.geometry("400x200")
-# Add a intro label
-labelIntro = Label(window, text="Select the folder:")
-labelIntro.grid(column=0, row=0)
-# Add button select a folder
-btnSelectFolder = Button(window, text="Browse...", command=handleSelectFolder)
-btnSelectFolder.grid(column=1, row=0)
-# Add a path label
-labelPath = Label(window, text="Path:", anchor="w")
-labelPath.grid(column=0, row=1)
-# Add a selected path label
-labelSelectedPath = Label(window, text="N/A", anchor="w")
-labelSelectedPath.grid(column=1, row=1)
-# Entry text
-labelCutToText = Label(window, text="Trim to keyword: ", anchor="w")
-labelCutToText.grid(column=0, row=2)
+window.title("Gmail Generator v1.1")
+window.geometry("500x130")
+# Main mail
+labelGmail = Label(window, text="Main Gmail:")
+labelGmail.grid(column=0, row=0)
+entryMainMail = Entry(window, width=50)
+entryMainMail.grid(column=1, row=0)
+#  Generate
+btnGenerate = Button(window, text="Generate", command=processEmail)
+btnGenerate.grid(column=0, row=1)
+entryGenerateMail = Entry(window, width=50)
+entryGenerateMail.grid(column=1, row=1)
+btnCopy = Button(window, text="Copy", command=copyEmail)
+btnCopy.grid(column=2, row=1)
 
-cutToText = Entry(window)
-cutToText.grid(column=1, row=2)
-# including Sub-Folder
-isIncludeSubFolder = IntVar()
-cbSubFolder = Checkbutton(window, text="Including SubFolder", variable=isIncludeSubFolder)
-cbSubFolder.grid(column=0, row=3)
-# Add a rename button
-btnRename = Button(window, text="Rename all files", command=processImages)
-btnRename.grid(column=0, row=4)
-# Add label successfully
-labelSuccess = Label(window, text="")
-labelSuccess.grid(column=0, row=5)
+labelLastGenerate = Label(window, text="Last generate: ")
+labelLastGenerate.grid(column=0, row=2)
+
+labelLastTime = Label(window, text="N/A")
+labelLastTime.grid(column=1, row=2)
 
 window.mainloop()
