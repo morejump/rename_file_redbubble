79d370e75344d8e9aad97fe089c7199593eebf74
morejump <thaoluu>
2022-07-09 18:06:11 +0700

    Store error images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import threading
 from concurrent.futures import ThreadPoolExecutor, wait
 from tkinter import *
@@ -63,26 +64,45 @@ def onStart():
 def resizeImages():
     processLabel.configure(text="Processing")
     features = []
-    executor = ThreadPoolExecutor(10)
+    executor = ThreadPoolExecutor(int(threadEntry.get()))
     folder = originalEntry.get()
     for filename in os.listdir(folder):
         filepath = f"{folder}/{filename}"
         feature = executor.submit(resizeImage, filepath, filename)
         features.append(feature)
     wait(features)
-    processLabel.configure(text="All images were resized completely")
+
+    totalImage = len([name for name in os.listdir(folder) if os.path.isfile(os.path.join(folder, name))])
+    resizeFolder = resizeEntry.get()
+    successImage = len([name for name in os.listdir(resizeFolder) if os.path.isfile(os.path.join(resizeFolder, name))])
+    errorFolder = f'{folder}/error_images'
+    failImage = 0
+    if os.path.exists(errorFolder):
+        failImage = len([name for name in os.listdir(errorFolder) if os.path.isfile(os.path.join(errorFolder, name))])
+    processLabel.configure(text=f"Resize {totalImage} images: {successImage} succeed, {failImage} failed ")
     print("ALL DONE")
     return
 
 
 def resizeImage(filePath, filename):
-    print(f"starting crop {filename}")
-    originalImage = Image.open(filePath)
-    cropImage = originalImage.crop(originalImage.getbbox())
-    if cropImage.size[0] >= cropImage.size[1]:
-        resizeBaseWith(cropImage, filename)
-    else:
-        resizeBaseHeight(cropImage, filename)
+    try:
+        print(f"starting crop {filename}")
+        originalImage = Image.open(filePath)
+        cropImage = originalImage.crop(originalImage.getbbox())
+        if cropImage.size[0] >= cropImage.size[1]:
+            resizeBaseWith(cropImage, filename)
+        else:
+            resizeBaseHeight(cropImage, filename)
+    except Exception as ex:
+        print(f"Image {filename}: {ex}")
+        currentFolder = originalEntry.get()
+        errorFolder = f'{currentFolder}/error_images'
+        if not os.path.exists(errorFolder):
+            os.makedirs(errorFolder)
+        shutil.copy(filePath, errorFolder)
+    finally:
+        originalImage.close()
+        cropImage.close()
     return
 
 
@@ -90,27 +110,33 @@ if __name__ == '__main__':
     MAX_WITH = 4500
     MAX_HEIGHT = 5400
     window = Tk()
-    window.title("Redbubble Standardizer v1.1")
+    window.title("Redbubble Standardizer v1.2")
     window.geometry("800x200")
     # an original section
     originalLabel = Label(window, text="Original folder")
     originalLabel.grid(column=0, row=0, sticky=W)
     originalEntry = Entry(window, width=80)
-    originalEntry.grid(column=1, row=0)
+    originalEntry.grid(column=1, row=0, sticky=W)
     btnSelectOriginal = Button(window, text="Browse...", command=handleSelectOriginalFolder)
     btnSelectOriginal.grid(column=2, row=0)
     # a resize section
     resizeLabel = Label(window, text="Resize folder")
     resizeLabel.grid(column=0, row=1, sticky=W)
     resizeEntry = Entry(window, width=80)
-    resizeEntry.grid(column=1, row=1)
+    resizeEntry.grid(column=1, row=1, sticky=W)
     btnSelectResize = Button(window, text="Browse...", command=handleSelectResizeFolder)
     btnSelectResize.grid(column=2, row=1)
+    # thread
+    threadLabel = Label(window, text="Thread numbers")
+    threadLabel.grid(column=0, row=2, sticky=W)
+    threadEntry = Entry(window, width=10)
+    threadEntry.grid(column=1, row=2, sticky=W)
+    threadEntry.insert(0, "5")
     # process section
     btnProcess = Button(window, text="Start", width=10, command=onStart)
-    btnProcess.grid(column=0, row=2, sticky=W)
+    btnProcess.grid(column=0, row=3, sticky=W)
     # process section
     processLabel = Label(window, text="")
-    processLabel.grid(column=0, row=3, sticky=W)
+    processLabel.grid(column=0, row=4, sticky=W)
 
     window.mainloop()
