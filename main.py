bb4dfeff8e623038c97c7844851d73ec61860959
morejump <thaoluu>
2022-09-14 15:14:13 +0700

    Update remove duplicaate functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import threading
+import re
 from tkinter import *
 from tkinter import filedialog
 from os.path import isfile, join
@@ -25,64 +26,30 @@ class MainWindow(QMainWindow, layout.Ui_MainWindow):
         if dlg.exec_():
             folderNames = dlg.selectedFiles()
             self.txtFolderPath.setText(folderNames[0])
-            self.lblMessage.setText("")
         return
 
-
-def divideFolderImages():
-    count = 0
-    folder = mainWindow.txtFolderPath.text()
-    onlyfiles = [file for file in os.listdir(folder) if isfile(join(folder, file))]
-    print(onlyfiles)
-    _numberImage = int(mainWindow.edtNumberImages.text())
-    while len(onlyfiles) != 0:
-        count += 1
-        numberAddImage = 0
-        if (len(onlyfiles)) < _numberImage:
-            numberAddImage = len(onlyfiles)
-        else:
-            numberAddImage = _numberImage
-
-        newDirectory = f"acc_{count}"
-        path = os.path.join(folder, newDirectory)
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-        for index in range(numberAddImage):
-            shutil.move(f"{folder}/{onlyfiles[index]}", f"{path}/{onlyfiles[index]}")
-        onlyfiles = [file for file in os.listdir(folder) if isfile(join(folder, file))]
-    mainWindow.lblMessage.setText("Successfully divided images")
+def removeDuplicate():
+    folderPath = mainWindow.txtFolderPath.text()
+    images = [file for file in os.listdir(folderPath) if isfile(join(folderPath, file))]
+    for image in images:
+        print(image)
+        fileName = image.split(".")[0]
+        hasEndNumber = re.search(r"_\d$", fileName)
+        hasCopyText = "- Copy" in image
+        if hasEndNumber or hasCopyText:
+            os.remove(f"{folderPath}/{image}")
     return
 
 
-def undivideFolderImages():
-    mainFolder = mainWindow.txtFolderPath.text()
-    for path, subdirs, files in os.walk(mainFolder):
-        for name in files:
-            filePath = os.path.join(path, name)
-            shutil.move(filePath, mainFolder)
-
-    folders = list(os.walk(mainFolder))[1:]
-    for folder in folders:
-        if not folder[2]:
-            os.rmdir(folder[0])
-    mainWindow.lblMessage.setText("Successfully undivided images")
+def processRemoveDuplicate():
+    threading.Thread(target=removeDuplicate).start()
     return
 
-
-def processDivideFolder():
-    threading.Thread(target=divideFolderImages).start()
-    return
-
-def processUndivideFolder():
-    threading.Thread(target=undivideFolderImages).start()
-    return
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = MainWindow()
-    mainWindow.setWindowTitle("Divide Folder v1.2")
-    mainWindow.btnDivide.clicked.connect(processDivideFolder)
-    mainWindow.btnUndivide.clicked.connect(processUndivideFolder)
+    mainWindow.setWindowTitle("Duplicate Remover v1.1")
+    mainWindow.btnRemoveDuplicate.clicked.connect(processRemoveDuplicate)
     mainWindow.show()
     app.exec_()
