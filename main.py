223f7d0a432cd5972f08126732db56a56e88ce6d
morejump <thaoluu>
2022-10-15 13:42:53 +0700

    Add Handling resize imatge parallely

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ import dbm.dumb
 
 
 def resizeBaseWith(image, filename):
-    print(f"Resizing {filename}")
     basewidth = MAX_WITH
     wpercent = (basewidth / float(image.size[0]))
     hsize = min(int((float(image.size[1]) * float(wpercent))), MAX_HEIGHT)
@@ -27,14 +26,12 @@ def resizeBaseWith(image, filename):
     xBasis = int((backgroundTrans.size[0] - image.size[0]) / 2)
     yBasis = int((backgroundTrans.size[1] - image.size[1]) / 2)
     backgroundTrans.paste(image, (xBasis, yBasis))
-    resizeFolder = resizeEntry.get()
+    resizeFolder = mainWindow.edtResizeFolder.text()
     backgroundTrans.save(f"{resizeFolder}/{filename}")
-    print(f"Complete resize {filename}")
     return
 
 
 def resizeBaseHeight(image, filename):
-    print(f"Resizing {filename}")
     baseheight = MAX_HEIGHT
     hpercent = (baseheight / float(image.size[1]))
     wsize = min(int((float(image.size[0]) * float(hpercent))), MAX_WITH)
@@ -43,43 +40,53 @@ def resizeBaseHeight(image, filename):
     xBasis = int((backgroundTrans.size[0] - image.size[0]) / 2)
     yBasis = int((backgroundTrans.size[1] - image.size[1]) / 2)
     backgroundTrans.paste(image, (xBasis, yBasis))
-    resizeFolder = resizeEntry.get()
+    resizeFolder = mainWindow.edtResizeFolder.text()
     backgroundTrans.save(f"{resizeFolder}/{filename}")
-    print(f"Complete resize {filename}")
     return
 
 
 def onStart():
-    threading.Thread(target=resizeImages).start()
+    global isStart
+    isStart = not isStart
+    if isStart:
+        logging.info("Starting")
+        mainWindow.btnStart.setText("Stop")
+        mainWindow.btnStart.setStyleSheet("background-color : red; color: white;")
+        threading.Thread(target=resizeImages).start()
+    else:
+        threadPool.clear()
+        logging.info("Stopping")
+        mainWindow.btnStart.setText("Start")
+        mainWindow.btnStart.setStyleSheet("background-color : green; color: white;")
     return
 
 
 def resizeImages():
-    processLabel.configure(text="Processing")
-    features = []
-    executor = ThreadPoolExecutor(int(threadEntry.get()))
-    folder = originalEntry.get()
+    global threadPool
+    threadPool.setMaxThreadCount(int(mainWindow.edtParallelHandling.text()))
+    logging.info("Starting resize images")
+
+    folder = mainWindow.edtOriginalFolder.text()
     for filename in os.listdir(folder):
         filepath = f"{folder}/{filename}"
-        feature = executor.submit(resizeImage, filepath, filename)
-        features.append(feature)
-    wait(features)
+        threadPool.start(ResizeImageTask(filepath, filename))
+
+    threadPool.waitForDone()
 
     totalImage = len([name for name in os.listdir(folder) if os.path.isfile(os.path.join(folder, name))])
-    resizeFolder = resizeEntry.get()
+    resizeFolder = mainWindow.edtResizeFolder.text()
     successImage = len([name for name in os.listdir(resizeFolder) if os.path.isfile(os.path.join(resizeFolder, name))])
     errorFolder = f'{folder}/error_images'
     failImage = 0
     if os.path.exists(errorFolder):
         failImage = len([name for name in os.listdir(errorFolder) if os.path.isfile(os.path.join(errorFolder, name))])
-    processLabel.configure(text=f"Resize {totalImage} images: {successImage} succeed, {failImage} failed ")
-    print("ALL DONE")
+    logging.info(f"Try to resize {totalImage} images: {successImage} succeed, {failImage} failed ")
     return
 
 
 def resizeImage(filePath, filename):
     try:
-        print(f"starting crop {filename}")
+        logging.info(f"starting crop {filename}")
         originalImage = Image.open(filePath)
         cropImage = originalImage.crop(originalImage.getbbox())
         if cropImage.size[0] >= cropImage.size[1]:
@@ -87,7 +94,7 @@ def resizeImage(filePath, filename):
         else:
             resizeBaseHeight(cropImage, filename)
     except Exception as ex:
-        print(f"Image {filename}: {ex}")
+        logging.info(f"Image {filename}: {ex}")
         currentFolder = originalEntry.get()
         errorFolder = f'{currentFolder}/error_images'
         if not os.path.exists(errorFolder):
@@ -97,6 +104,17 @@ def resizeImage(filePath, filename):
         originalImage.close()
         cropImage.close()
     return
+
+
+class ResizeImageTask(QRunnable):
+
+    def __init__(self, filePath, filename):
+        super(ResizeImageTask, self).__init__()
+        self.filePath = filePath
+        self.filename = filename
+
+    def run(self):
+        resizeImage(self.filePath, self.filename)
 
 
 class MainWindow(QMainWindow, layout.Ui_MainWindow):
@@ -110,7 +128,8 @@ class MainWindow(QMainWindow, layout.Ui_MainWindow):
 
     def openOriginalFolder(self):
         try:
-            folderPath = QFileDialog.getExistingDirectory(None, 'Select a original folder:', 'C:\\', QFileDialog.ShowDirsOnly)
+            folderPath = QFileDialog.getExistingDirectory(None, 'Select a original folder:', 'C:\\',
+                                                          QFileDialog.ShowDirsOnly)
             logging.info(folderPath)
             if folderPath:
                 self.edtOriginalFolder.setText(folderPath)
@@ -120,7 +139,8 @@ class MainWindow(QMainWindow, layout.Ui_MainWindow):
 
     def openResizeFolder(self):
         try:
-            folderPath = QFileDialog.getExistingDirectory(None, 'Select a resize folder:', 'C:\\', QFileDialog.ShowDirsOnly)
+            folderPath = QFileDialog.getExistingDirectory(None, 'Select a resize folder:', 'C:\\',
+                                                          QFileDialog.ShowDirsOnly)
             logging.info(folderPath)
             if folderPath:
                 self.edtResizeFolder.setText(folderPath)
@@ -184,7 +204,7 @@ def displayMessage(msg: str):
     global displayMsgNumber
     print(msg)
     displayMsgNumber += 1
-    if "Successfully" in msg:
+    if "succeed" in msg:
         formatColor = '<span style="color:green;">{}</span>'
         mainWindow.edtLogs.appendHtml(formatColor.format(msg))
     else:
@@ -208,7 +228,12 @@ def savePreferences():
 
 if __name__ == '__main__':
 
+    isStart = False
+    MAX_WITH = 4500
+    MAX_HEIGHT = 5400
+
     db = dbm.open('mydb', 'c')
+
     displayMsgNumber = 0
     KEY_ACTIVE_KEY = "KEY_ACTIVE_KEY"
     ORIGINAL_FOLDER_PATH_KEY = "ORIGINAL_FOLDER_PATH_KEY"
@@ -232,6 +257,9 @@ if __name__ == '__main__':
     mainWindow = MainWindow()
     mainWindow.setWindowTitle("Redbubble resizer v1.0")
     intValidator = PyQt5.QtGui.QIntValidator()
+    #  threadpool
+    threadPool = QThreadPool.globalInstance()
+    threadPool.clear()
     # setup logs
     logSignal = LogSignal()
     logTextBox = QTextEditLogger()
@@ -256,6 +284,7 @@ if __name__ == '__main__':
     mainWindow.edtParallelHandling.setText(db.get(PARALLEL_HANDLING_KEY).decode("utf-8"))
     #
     mainWindow.edtParallelHandling.setValidator(intValidator)
+    mainWindow.btnStart.clicked.connect(onStart)
 
     # setup active key dialog
     activeKeyDialog = QtWidgets.QDialog()
